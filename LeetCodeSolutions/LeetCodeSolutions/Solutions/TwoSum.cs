3c82a8e0dbcd57a6bdd0b5d7fcda794439b7eb32
ToshmatovDeveloper <toshmatovdeveloper1105@gmail.com>
2026-07-07 10:20:08 +0500

    TwoSum problem solution.

---
+++ LeetCodeSolutions/LeetCodeSolutions/Solutions/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions.Solutions;
+
+public class TwoSum
+{
+    public int[] Solution(int[] nums, int target) 
+    {    
+        int first = 0;
+        int second = 0;
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    first = j;
+                    second = i;
+                }
+            }
+        }
+        
+        return [first, second]; 
+    }
+}
