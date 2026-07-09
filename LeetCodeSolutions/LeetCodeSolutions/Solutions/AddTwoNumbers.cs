a8772690b13477a3c4794b1618a79bc178fd1cb5
Iqbol Toshmatov <toshmatovdeveloper1105@gmail.com>
2026-07-09 15:10:40 +0300

    Add solution for Add Two Numbers problem

---
+++ LeetCodeSolutions/LeetCodeSolutions/Solutions/AddTwoNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeSolutions.Solutions;
+
+public class AddTwoNumbers
+{
+    public class ListNode(int val = 0, ListNode next = null)
+    {
+         public int val = val;
+         public ListNode next = next;
+    }
+    
+    public ListNode Solution(ListNode l1, ListNode l2)
+    {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+    
+        int carry = 0; 
+
+        while (l1 != null || l2 != null || carry != 0)
+        {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+        
+            carry = sum / 10;       
+            int digit = sum % 10;   
+
+            current.next = new ListNode(digit);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
+    }
+
+}
